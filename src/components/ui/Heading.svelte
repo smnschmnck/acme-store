379e221f9685401fa50bcb573c15948ec0ad81e2
smnschmnck <simon.schmeinck@gmx.de>
2023-09-04 22:55:47 +0200

    Add logo

---
+++ src/components/ui/Heading.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { twMerge } from 'tailwind-merge';
+	interface $$Props extends Partial<HTMLHeadingElement> {
+		className?: string;
+	}
+	export let className: string = '';
+</script>
+
+<h2 class={twMerge("w-fit font-['Poppins'] text-3xl font-bold", className)} {...$$restProps}>
+	<slot />
+</h2>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,16 @@
 <script>
 	import '../app.css';
+	import Heading from '../components/ui/Heading.svelte';
 </script>
 
-<slot />
+<div>
+	<header class="border-b border-b-zinc-200 px-12 py-6">
+		<div class="flex gap-1">
+			<Heading className="text-2xl">acme</Heading>
+			<Heading className="font-normal text-2xl">store</Heading>
+		</div>
+	</header>
+	<div class="px-12">
+		<slot />
+	</div>
+</div>
