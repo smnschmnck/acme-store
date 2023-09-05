d09883d346482f5506b42d612f1ea95bd3d5fca6
smnschmnck <simon.schmeinck@gmx.de>
2023-09-06 00:06:08 +0200

    Small styling refactor

--- src/components/ui/SearchBar.svelte
+++ src/components/ui/SearchBar.svelte
@@ -1,23 +1,20 @@
 <script>
 	import MagnifyingGlass from '../../assets/icons/MagnifyingGlass.svelte';
 	import Button from './Button.svelte';
+	import Select from './Select.svelte';
 </script>
 
 <form
 	method="get"
 	autocomplete="off"
-	class="flex h-10 rounded-lg bg-zinc-100 focus-within:outline focus-within:outline-1 focus-within:outline-blue-500"
+	class="flex h-10 overflow-hidden rounded-lg bg-zinc-100 focus-within:outline focus-within:outline-1 focus-within:outline-blue-500"
 >
-	<select
-		class="w-28 rounded-l-lg bg-zinc-200 text-sm font-medium outline-none focus-visible:outline focus-visible:outline-2 focus-visible:outline-blue-500"
-		name="category"
-		id="category-select"
-	>
+	<Select className="rounded-none w-28">
 		<option value="All">All</option>
 		<option value="Shoes">Shoes</option>
 		<option value="Pants">Pants</option>
 		<option value="T-Shirts">T-Shirts</option>
-	</select>
+	</Select>
 	<div class="flex w-72 items-center justify-center gap-1 px-2">
 		<div class="text-zinc-500">
 			<MagnifyingGlass />
@@ -29,5 +26,5 @@
 			class="h-full w-full bg-transparent text-sm placeholder-zinc-500 outline-none placeholder:text-sm"
 		/>
 	</div>
-	<Button type="submit" className="rounded-l-none">Search</Button>
+	<Button type="submit" className="rounded-none">Search</Button>
 </form>
---
+++ src/components/ui/Select.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	import { twMerge } from 'tailwind-merge';
+
+	interface $$Props extends Partial<HTMLSelectElement> {
+		className?: string;
+	}
+
+	export let className = '';
+</script>
+
+<select
+	class={twMerge(
+		'w-full rounded-lg bg-zinc-200 text-sm font-medium outline-none focus-visible:outline focus-visible:outline-2 focus-visible:outline-blue-500',
+		className
+	)}
+	name="category"
+	id="category-select"
+>
+	<slot />
+</select>
+
+<style>
+	/* Overwrite uggo default css */
+	select {
+		appearance: none;
+		-webkit-appearance: none;
+		-moz-appearance: none;
+		background-image: url('/icons/chevron-down.svg');
+		background-position-x: calc(100% - 1rem); /* Adjust as needed */
+		background-position-y: center;
+		background-repeat: no-repeat;
+		padding-right: 2.5rem; /* This padding adds space for the arrow */
+		padding-left: 1rem;
+	}
+</style>
