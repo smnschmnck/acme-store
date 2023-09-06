8af8c1dae6f076f9a7342ca095d18207f04eebfd
smnschmnck <simon.schmeinck@gmx.de>
2023-09-06 02:00:42 +0200

    Add input component

---
+++ src/components/ui/Input.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import { twMerge } from 'tailwind-merge';
+
+	interface $$Props extends Partial<HTMLInputElement> {
+		className?: string;
+	}
+
+	export let className = '';
+</script>
+
+<div
+	class="flex h-10 w-80 items-center justify-center gap-1 rounded-lg bg-zinc-100 px-3 focus-within:outline focus-within:outline-2 focus-within:outline-blue-500"
+>
+	<div class="text-zinc-500">
+		<slot name="icon" />
+	</div>
+	<input
+		class={twMerge(
+			'h-full w-full bg-transparent text-sm placeholder-zinc-500 outline-none placeholder:text-sm',
+			className
+		)}
+		{...$$restProps}
+	/>
+</div>
