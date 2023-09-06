1bb71467bc8126c7ba7a1ffe76c4419dd88375e6
smnschmnck <simon.schmeinck@gmx.de>
2023-09-06 02:18:48 +0200

    Add flame thingy

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,14 @@
 <script>
+	import GradientFlame from '../assets/icons/GradientFlame.svelte';
 	import Heading from '../components/ui/Heading.svelte';
 </script>
 
 <div class="flex flex-col gap-6 pt-10">
-	<Heading>Trending Products</Heading>
+	<div class="flex items-center gap-2">
+		<div class="h-6 w-6">
+			<GradientFlame />
+		</div>
+		<Heading>Trending Products</Heading>
+	</div>
 	<div class="flex h-32 gap-2 bg-zinc-100 p-2">todo</div>
 </div>
