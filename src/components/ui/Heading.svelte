9b0c9be9a3ef250d302389f015788fa26aff47f0
smnschmnck <simon.schmeinck@gmx.de>
2023-09-08 18:49:15 +0200

    Install eslint for tailwind + fix lint errors

--- src/components/ui/Heading.svelte
+++ src/components/ui/Heading.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
 	import { twMerge } from 'tailwind-merge';
+
 	interface $$Props extends Partial<HTMLHeadingElement> {
 		className?: string;
 	}
-	export let className: string = '';
+	export let className = '';
 </script>
 
 <h2 class={twMerge("w-fit font-['Poppins'] text-2xl font-bold", className)} {...$$restProps}>
