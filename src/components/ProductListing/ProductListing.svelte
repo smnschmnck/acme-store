11ad3a352de5d5b6b56d0dc742ada58605e2b8a5
smnschmnck <simon.schmeinck@gmx.de>
2023-09-09 15:44:00 +0200

    Put seller info below picture on mobile

--- src/components/ProductListing/ProductListing.svelte
+++ src/components/ProductListing/ProductListing.svelte
@@ -1,17 +1,17 @@
 <script lang="ts">
 	import type { Sellers } from '../../db/schema';
+	import SellerInfo from './SellerInfo.svelte';
 	import StarRating from './StarRating.svelte';
 
 	export let id: string;
 	export let name: string;
 	export let price: string;
-	export let category: string;
 	export let rating: string;
 	export let seller: Sellers;
 </script>
 
-<div class="flex h-40 w-full justify-between gap-2 bg-white p-4">
-	<div class="flex w-full gap-4">
+<div class="flex h-40 w-full flex-col justify-between gap-2 bg-white p-4 sm:flex-row">
+	<div class="flex h-full w-full gap-4">
 		<div class="aspect-square h-full rounded-lg bg-zinc-300" />
 		<div class="flex h-full w-full flex-col justify-between overflow-hidden lg:w-fit">
 			<div class="flex flex-col gap-0.5 text-sm">
@@ -21,18 +21,23 @@
 				<StarRating rating={Number(rating)} />
 			</div>
 			<h4 class="block text-sm font-bold lg:flex">{price}€</h4>
-			<a
-				href={`/seller/${seller.id}`}
-				class="flex max-w-fit gap-2 truncate rounded-lg bg-zinc-100 p-2 text-sm font-medium transition hover:bg-zinc-200"
-			>
-				<h5 class="truncate">
-					{seller.name}
-				</h5>
-				<p class="text-zinc-500">
-					{seller.rating}% ({seller.ratingCount})
-				</p>
-			</a>
+			<div class="hidden sm:block">
+				<SellerInfo
+					id={seller.id}
+					name={seller.name}
+					rating={seller.rating}
+					ratingCount={seller.ratingCount}
+				/>
+			</div>
 		</div>
+	</div>
+	<div class="flex sm:hidden">
+		<SellerInfo
+			id={seller.id}
+			name={seller.name}
+			rating={seller.rating}
+			ratingCount={seller.ratingCount}
+		/>
 	</div>
 	<h4 class="hidden text-sm font-bold lg:block">{price}€</h4>
 </div>
---
+++ src/components/ProductListing/SellerInfo.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	export let id: string;
+	export let name: string;
+	export let rating: string;
+	export let ratingCount: number;
+</script>
+
+<a
+	href={`/seller/${id}`}
+	class="flex h-fit max-w-fit gap-2 truncate rounded-lg bg-zinc-100 p-2 text-sm font-medium transition hover:bg-zinc-200 sm:justify-start"
+>
+	<h5 class="truncate">
+		{name}
+	</h5>
+	<p class="text-zinc-500">
+		{rating}% ({ratingCount})
+	</p>
+</a>
