ecb3afce4546facb875613242ab1845672ca3a19
smnschmnck <simon.schmeinck@gmx.de>
2023-09-09 15:33:28 +0200

    Remove category in favor of longer product name

--- src/components/ProductListing/ProductListing.svelte
+++ src/components/ProductListing/ProductListing.svelte
@@ -13,16 +13,17 @@
 <div class="flex h-40 w-full justify-between gap-2 bg-white p-4">
 	<div class="flex w-full gap-4">
 		<div class="aspect-square h-full rounded-lg bg-zinc-300" />
-		<div class="flex h-full w-full flex-col justify-between truncate lg:w-fit">
+		<div class="flex h-full w-full flex-col justify-between overflow-hidden lg:w-fit">
 			<div class="flex flex-col gap-0.5 text-sm">
-				<a href={`/item/${id}`} class="truncate font-bold hover:underline">{name}</a>
-				<p class="font-medium text-zinc-500">{category}</p>
+				<a href={`/item/${id}`} class="line-clamp-2 text-ellipsis font-bold hover:underline"
+					>{name}</a
+				>
 				<StarRating rating={Number(rating)} />
 			</div>
 			<h4 class="block text-sm font-bold lg:flex">{price}€</h4>
 			<a
 				href={`/seller/${seller.id}`}
-				class="flex max-w-fit gap-2 rounded-lg bg-zinc-100 p-2 text-sm font-medium transition hover:bg-zinc-200"
+				class="flex max-w-fit gap-2 truncate rounded-lg bg-zinc-100 p-2 text-sm font-medium transition hover:bg-zinc-200"
 			>
 				<h5 class="truncate">
 					{seller.name}
