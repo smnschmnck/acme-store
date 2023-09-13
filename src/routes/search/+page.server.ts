c092cd81466945aa43368e27d7ac17ba757a768b
smnschmnck <simon.schmeinck@gmx.de>
2023-09-14 00:13:01 +0200

    Filter products based on search params

--- src/routes/search/+page.server.ts
+++ src/routes/search/+page.server.ts
@@ -1,18 +1,33 @@
-import { and, like } from 'drizzle-orm';
+import { and, eq, gt, like, lt } from 'drizzle-orm';
 import type { PageServerLoad } from '../$types';
 import { db } from '../../db/connection';
 import { products } from '../../db/schema';
 import { sortings } from './utils/sortings';
 import { extractSearchParams, type SearchDataParams } from './utils/searchParams';
 
-const getProducts = async ({ category, sorting, searchQuery }: SearchDataParams) => {
+const getProducts = async ({
+	category,
+	searchQuery,
+	sorting,
+	productRating,
+	minPrice,
+	maxPrice,
+	minSellerRating,
+	maxSellerRating,
+	filterCategories
+}: SearchDataParams) => {
 	const hasCategory = category !== 'All';
-	sorting ??= 'popular';
 
-	return await db.query.products.findMany({
+	const categoryQuery = filterCategories.map((c) => eq(products.category, c));
+
+	const productsArray = await db.query.products.findMany({
 		where: and(
 			like(products.name, `%${searchQuery}%`),
-			hasCategory ? like(products.category, `%${category}%`) : undefined
+			hasCategory ? like(products.category, `%${category}%`) : undefined,
+			productRating ? gt(products.rating, productRating) : undefined,
+			minPrice ? gt(products.price, minPrice) : undefined,
+			maxPrice ? lt(products.price, maxPrice) : undefined,
+			...categoryQuery
 		),
 		with: {
 			seller: true
@@ -20,6 +35,26 @@ const getProducts = async ({ category, sorting, searchQuery }: SearchDataParams)
 		orderBy: sortings[sorting].config,
 		limit: 25
 	});
+
+	//I only have to do this manually because for some reason drizzle keeps me from doing this in the ORM
+	//TODO: find a way to do this with Drizzle without loosing typesafety
+	if (minSellerRating || maxSellerRating) {
+		return productsArray.filter((product) => {
+			if (minSellerRating) {
+				if (Number(product.seller.rating) < Number(minSellerRating)) {
+					return false;
+				}
+			}
+			if (maxSellerRating) {
+				if (Number(product.seller.rating) > Number(maxSellerRating)) {
+					return false;
+				}
+			}
+			return true;
+		});
+	}
+
+	return productsArray;
 };
 
 export const load: PageServerLoad = ({ url }) => {
