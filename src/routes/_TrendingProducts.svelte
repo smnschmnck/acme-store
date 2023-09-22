054e915f991e7075a5ec09280ee1ce7ff04afb53
smnschmnck <simon.schmeinck@gmx.de>
2023-09-22 15:18:49 +0200

    Make trending products grid responsive

--- src/routes/_TrendingProducts.svelte
+++ src/routes/_TrendingProducts.svelte
@@ -14,8 +14,8 @@
 		<GradientFlame dimensions="h-6 w-6" />
 		<Heading>Trending products</Heading>
 	</div>
-	<div class="flex flex-col gap-2 bg-zinc-100 p-2 lg:flex-row">
-		<div class="grid w-full grid-cols-2 grid-rows-2 gap-2">
+	<div class="bg-zinc-100 p-2">
+		<div class="grid w-full grid-cols-1 grid-rows-4 gap-2 lg:grid-cols-2 lg:grid-rows-2">
 			{#each firstFour as p}
 				<ProductListing
 					id={p.id}
