5caff6b593319dd4e66244599e68d4684d76a43b
smnschmnck <simon.schmeinck@gmx.de>
2023-10-03 16:49:14 +0200

    Make product page more usable on mobile

--- src/components/ImageCarousel.svelte
+++ src/components/ImageCarousel.svelte
@@ -2,6 +2,7 @@
 	export let images: { src: string; alt: string }[];
 
 	let safeImages = images.slice(0, 10);
+	export let imageIdPrefix: string;
 </script>
 
 <div class="flex max-w-fit flex-col gap-4">
@@ -12,7 +13,7 @@
 			class="flex snap-x snap-mandatory scroll-m-4 overflow-hidden overflow-x-auto scroll-smooth"
 		>
 			{#each safeImages as image, i}
-				<div id={`image-${i}`} class="max-h-full min-h-full min-w-full max-w-full">
+				<div id={`${imageIdPrefix}-${i}`} class="max-h-full min-h-full min-w-full max-w-full">
 					<img class="snap-center" src={image.src} alt={image.alt} />
 				</div>
 			{/each}
@@ -22,7 +23,7 @@
 	<div class="flex flex-wrap gap-2 lg:max-w-[24rem] lg:gap-4">
 		{#each safeImages as preview, i}
 			<a
-				href={`#image-${i}`}
+				href={`#${imageIdPrefix}-${i}`}
 				class="flex aspect-square w-16 items-center justify-center overflow-hidden bg-zinc-100 transition-all hover:scale-110"
 			>
 				<img src={preview.src} alt={preview.alt} />
--- src/routes/item/[slug]/+page.svelte
+++ src/routes/item/[slug]/+page.svelte
@@ -25,16 +25,18 @@
 <div class="py-10">
 	{#if product}
 		<div class="flex flex-col gap-8 md:flex-row">
-			<ImageCarousel images={exampleImages} />
+			<div class="hidden md:block">
+				<ImageCarousel images={exampleImages} imageIdPrefix={'imgD'} />
+			</div>
 			<div class="flex w-full flex-col gap-10">
-				<div class="flex w-full justify-between border-b border-b-zinc-200 pb-10 text-sm">
+				<div class="flex w-full justify-between border-b-zinc-200 text-sm md:border-b md:pb-10">
 					<div class="flex flex-col gap-6">
 						<div class="flex flex-col">
 							<h1 class="font-bold">{product.name}</h1>
 							<p class="font-medium text-zinc-500">{product.category}</p>
 							<StarRating rating={Number(product.rating)} />
 						</div>
-						<p class="text-xl font-bold">{product.price}€</p>
+						<p class="hidden text-xl font-bold md:block">{product.price}€</p>
 					</div>
 					<SellerInfo
 						id={product.sellerId}
@@ -43,6 +45,10 @@
 						ratingCount={product.seller.ratingCount}
 					/>
 				</div>
+				<div class="block md:hidden">
+					<ImageCarousel imageIdPrefix={'imgM'} images={exampleImages} />
+				</div>
+				<p class="block text-xl font-bold md:hidden">{product.price}€</p>
 				<div class="flex w-full gap-4">
 					<div class="w-48">
 						<Input placeholder="Amount" value="1" className="text-center" type="number" />
