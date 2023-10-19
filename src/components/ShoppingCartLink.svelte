3d9fcc311213485d67d23a29a0f3986b4a36ac27
smnschmnck <simon.schmeinck@gmx.de>
2023-10-19 18:27:22 +0200

    Only show shopping cart product count when its > 0

--- src/components/ShoppingCartLink.svelte
+++ src/components/ShoppingCartLink.svelte
@@ -8,9 +8,11 @@
 	href="/shopping-cart"
 	class="relative flex h-10 w-10 items-center justify-center rounded-lg p-2 transition hover:bg-zinc-100"
 >
-	<span
-		class="absolute right-0 top-1 flex h-4 w-4 items-center justify-center rounded-full bg-red-500 text-xs font-medium text-white"
-		>{productCount}</span
-	>
+	{#if productCount > 0}
+		<span
+			class="absolute right-0 top-1 flex h-4 w-4 items-center justify-center rounded-full bg-red-500 text-xs font-medium text-white"
+			>{productCount}</span
+		>
+	{/if}
 	<ShoppingCart />
 </a>
