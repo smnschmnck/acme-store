6d42ab9e07d6dcea8095f292b7ed5cdb5a2629ae
smnschmnck <simon.schmeinck@gmx.de>
2023-10-28 18:51:16 +0200

    Use <dialog> for modal

--- src/components/ui/Modal.svelte
+++ src/components/ui/Modal.svelte
@@ -1,14 +1,3 @@
-<script lang="ts">
-	export let id: string;
-	export let mobileFullscreen = false;
-</script>
-
-<input type="checkbox" {id} checked class="peer hidden" />
-<label
-	for={id}
-	class="absolute right-0 top-0 hidden h-full w-full cursor-default items-center justify-center bg-white/50 backdrop-blur-[2px] peer-checked:flex"
->
-	<label class={mobileFullscreen ? 'h-full w-full sm:h-fit sm:w-fit' : ''} for="none">
-		<slot />
-	</label>
-</label>
+<dialog class="fixed right-0 top-0 h-full w-full bg-white/50 backdrop-blur-sm sm:absolute" open>
+	<slot />
+</dialog>
--- src/routes/item/[slug]/+page.svelte
+++ src/routes/item/[slug]/+page.svelte
@@ -9,6 +9,7 @@
 	import type { PageData } from './$types';
 	import exampleTShirt from './t-shirt-1.avif';
 	import Modal from '../../../components/ui/Modal.svelte';
+	import ButtonSecondary from '../../../components/ui/ButtonSecondary.svelte';
 
 	export let data: PageData;
 	const { product } = data;
@@ -27,17 +28,21 @@
 
 <div class="py-10">
 	{#if addedToCart === 'success'}
-		<Modal mobileFullscreen id="shoppingCartSuccessModal">
-			<div
-				class="flex h-full w-full flex-col gap-6 border-zinc-200 bg-white px-20 py-16 sm:rounded-xl sm:border"
-			>
-				<div>
-					<h1 class="text-xl font-medium">Successfully added product to cart</h1>
-					<p class="text-sm text-zinc-500">{product?.name} successfully added to your cart</p>
-				</div>
-				<div class="flex w-full flex-col justify-center gap-4 sm:flex-row-reverse">
-					<Link className="w-full" href="/shopping-cart">Go to cart</Link>
-					<LinkSecondary href={product?.id ?? '/'} className="w-full">Keep shopping</LinkSecondary>
+		<Modal>
+			<div class="flex h-full w-full items-center justify-center">
+				<div
+					class="flex h-full w-full flex-col gap-6 border-zinc-200 bg-white px-20 py-16 sm:h-fit sm:w-fit sm:rounded-xl sm:border"
+				>
+					<div>
+						<h1 class="text-xl font-medium">Successfully added product to cart</h1>
+						<p class="text-sm text-zinc-500">{product?.name} successfully added to your cart</p>
+					</div>
+					<div class="flex w-full flex-col justify-center gap-4 sm:flex-row-reverse">
+						<Link className="w-full" href="/shopping-cart">Go to cart</Link>
+						<form method="dialog" class="w-full">
+							<ButtonSecondary className="w-full">Keep shopping</ButtonSecondary>
+						</form>
+					</div>
 				</div>
 			</div>
 		</Modal>
