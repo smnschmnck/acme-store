5360d15db3da08e397c1492c100ca194d4475efd
smnschmnck <simon.schmeinck@gmx.de>
2023-10-30 17:54:11 +0100

    Place modal in center

--- src/components/ui/Modal.svelte
+++ src/components/ui/Modal.svelte
@@ -1,3 +1,6 @@
-<dialog class="fixed right-0 top-0 h-full w-full bg-white/50 backdrop-blur-sm sm:absolute" open>
+<dialog
+	class="fixed right-0 top-0 z-50 h-full w-full overscroll-contain bg-white/50 backdrop-blur-sm"
+	open
+>
 	<slot />
 </dialog>
