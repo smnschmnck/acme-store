2b22af79eedcbd04c20a992f4149b9bf11673c7f
smnschmnck <simon.schmeinck@gmx.de>
2024-05-07 14:57:35 +0200

    Use fetch instead of axios

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -1,28 +1,31 @@
 import { drizzle } from 'drizzle-orm/mysql-proxy';
-import axios, { isAxiosError } from 'axios';
-import { error } from '@sveltejs/kit';
 import { env } from '$env/dynamic/private';
 
 export const db = drizzle(async (sql, params, method) => {
-	try {
-		const credentials = {
-			username: env.DATABASE_USERNAME,
-			password: env.DATABASE_PASSWORD
-		};
-		const rows = await axios.post(env.DATABASE_PROXY, {
-			sql,
-			params,
-			method,
-			credentials
-		});
+	const proxyUrl = `${env.DATABASE_PROXY_HOST}/query`;
 
-		return { rows: rows.data };
-	} catch (e: unknown) {
-		if (isAxiosError(e)) {
-			console.error('Error from mysql proxy server: ', e.response?.data);
-		} else {
-			console.error('Unknown error', error);
+	console.log('PARAMS', params);
+
+	const body = {
+		sql,
+		params,
+		method,
+		secretKey: env.DATABASE_PROXY_SECRET_KEY
+	};
+	const res = await fetch(proxyUrl, {
+		method: 'post',
+		body: JSON.stringify(body),
+		headers: {
+			Accept: 'application/json',
+			'Content-Type': 'application/json'
 		}
+	});
+
+	if (!res.ok) {
+		console.log('-------QUERY ERROR------', await res.text());
+
 		return { rows: [] };
 	}
+
+	return await res.json();
 });
