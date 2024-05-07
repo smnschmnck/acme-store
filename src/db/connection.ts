b3530e84ddebfc714b75d862f5964495bbc00560
smnschmnck <simon.schmeinck@gmx.de>
2024-05-07 15:45:50 +0200

    try post

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -5,11 +5,15 @@ import type * as Schema from '../db/schema';
 
 export const db = drizzle<typeof Schema>(async (sql, params, method) => {
 	const body = { sql, params, method };
+	const url = `${env.DATABASE_PROXY_HOST}/query`;
 	console.log('-----BODY-----', body);
-	console.log('-----URL-----', env.DATABASE_PROXY_HOST);
+	console.log('-----URL-----', url);
 
-	const res = await fetch(env.DATABASE_PROXY_HOST);
-	console.log('----GET----', await res.text());
+	const res = await fetch(url, {
+		method: 'post',
+		body: JSON.stringify(body)
+	});
+	console.log('----POST----', await res.text());
 
 	const out: any = null;
 	return out;
