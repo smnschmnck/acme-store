077971ee5fa850c15f1194ab149063ca80ab4049
smnschmnck <simon.schmeinck@gmx.de>
2024-05-07 15:06:19 +0200

    use example implementation

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -1,31 +1,13 @@
+import axios from 'axios';
 import { drizzle } from 'drizzle-orm/mysql-proxy';
-import { env } from '$env/dynamic/private';
 
 export const db = drizzle(async (sql, params, method) => {
-	const proxyUrl = `${env.DATABASE_PROXY_HOST}/query`;
+	try {
+		const rows = await axios.post('http://localhost:3000/query', { sql, params, method });
 
-	console.log('PARAMS', params);
-
-	const body = {
-		sql,
-		params,
-		method,
-		secretKey: env.DATABASE_PROXY_SECRET_KEY
-	};
-	const res = await fetch(proxyUrl, {
-		method: 'post',
-		body: JSON.stringify(body),
-		headers: {
-			Accept: 'application/json',
-			'Content-Type': 'application/json'
-		}
-	});
-
-	if (!res.ok) {
-		console.log('-------QUERY ERROR------', await res.text());
-
+		return { rows: rows.data };
+	} catch (e: any) {
+		console.error('Error from mysql proxy server: ', e.response.data);
 		return { rows: [] };
 	}
-
-	return await res.json();
 });
