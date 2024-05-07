61743fae64d2e14a974996602169e4405951b309
smnschmnck <simon.schmeinck@gmx.de>
2024-05-07 15:29:20 +0200

    use axios again

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -1,13 +1,14 @@
 import { env } from '$env/dynamic/private';
+import axios from 'axios';
 import { drizzle } from 'drizzle-orm/mysql-proxy';
 
 export const db = drizzle(async (sql, params, method) => {
-	const body = { sql, params, method };
-	const res = await fetch(`${env.DATABASE_PROXY_HOST}/query`, {
-		method: 'post',
-		body: JSON.stringify(body)
-	});
-	const rows = await res.json();
+	try {
+		const rows = await axios.post(`${env.DATABASE_PROXY_HOST}/query`, { sql, params, method });
 
-	return { rows };
+		return { rows: rows.data };
+	} catch (e: any) {
+		console.error('Error from mysql proxy server: ', e.response.data);
+		return { rows: [] };
+	}
 });
