83de1e1d185f5f4420a1dadbde180dd0501b589a
smnschmnck <simon.schmeinck@gmx.de>
2024-05-07 15:22:45 +0200

    use post

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -4,6 +4,7 @@ import { drizzle } from 'drizzle-orm/mysql-proxy';
 export const db = drizzle(async (sql, params, method) => {
 	const body = { sql, params, method };
 	const res = await fetch(env.DATABASE_PROXY_HOST, {
+		method: 'post',
 		body: JSON.stringify(body)
 	});
 	const rows = await res.json();
