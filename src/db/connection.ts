3bdc646b8f538f3c17706a743c15a3f1ef65ff3e
smnschmnck <simon.schmeinck@gmx.de>
2024-05-07 15:06:55 +0200

    use env vars

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -1,9 +1,10 @@
+import { env } from '$env/dynamic/private';
 import axios from 'axios';
 import { drizzle } from 'drizzle-orm/mysql-proxy';
 
 export const db = drizzle(async (sql, params, method) => {
 	try {
-		const rows = await axios.post('http://localhost:3000/query', { sql, params, method });
+		const rows = await axios.post(env.DATABASE_PROXY_HOST, { sql, params, method });
 
 		return { rows: rows.data };
 	} catch (e: any) {
